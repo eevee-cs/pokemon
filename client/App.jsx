dfae092e293de2fd5defa28981f3b5de26966aa3
alexchao26 <alexchaoo@gmail.com>
2019-11-25 14:44:40 -0500

    proof of concept for canvas, battle/overworld

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import OverWorld from './OverWorldComponents/OverWorld';
+import BattleWorld from './BattleWorldComponents/BattleWorld';
 
-const App = () => (
-  <h1>REACT APP</h1>
-);
+const App = () => {
+  // world is 0, battle is 1
+  // This hook creates worldOrBattle state that toggles between the two worlds
+  // the toggleToBattle/World prop method will trigger the toggle
+  const [worldOrBattle, setWorldOrBattle] = useState(1);
 
+  // canvasRef to be passed as a prop to provide access to the canvas to children components
+  const canvasRef = React.useRef(null);
+
+  // render
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        width={600}
+        height={600}
+      />
+
+      {
+        worldOrBattle && (
+          <OverWorld
+            canvasRef={canvasRef}
+            toggleToBattle={() => setWorldOrBattle(0)}
+          />
+        )
+      }
+      { !worldOrBattle && (
+        <BattleWorld
+          canvasRef={canvasRef}
+          toggleToWorld={() => setWorldOrBattle(1)}
+        />
+      )}
+    </>
+  );
+};
 export default App;
