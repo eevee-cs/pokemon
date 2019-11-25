c4ec0162cf141772ce3e5b87727e6c6a3f5b1c17
alexchao26 <alexchaoo@gmail.com>
2019-11-25 15:03:19 -0500

    initial dev commit

---
+++ client/BattleWorldComponents/BattleWorld.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class BattleWorld extends React.Component {
+  componentDidMount() {
+    const { canvasRef } = this.props;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.fillStyle = '#3a3acf';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Battle World</h1>
+        <button type="button" onClick={() => this.props.toggleToWorld()}>im a button</button>
+      </>
+
+    );
+  }
+}
+
+export default BattleWorld;
---
+++ client/OverWorldComponents/OverWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const OverWorld = ({ canvasRef, toggleToBattle }) => (
+  <>
+    <h1>OverWorld</h1>
+    <button type="button" onClick={() => toggleToBattle()}>im a button</button>
+  </>
+);
+
+export default OverWorld;
